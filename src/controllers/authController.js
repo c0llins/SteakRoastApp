ebb14c2b4828059f28e0da8aa6e21d7ed7b2f1b4
c0llins <collins.dev@icloud.com>
2019-09-03 16:32:55 -0300

    First

---
+++ src/controllers/authController.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/register', async(req, res) =>{
+    try{
+        const { email } = req.body;
+
+        if(await User.findOne({ email })){
+            return res.status(400).send({ error: 'User alredy exists!'})
+        }
+
+        const user = await User.create(req.body);
+        console.log("Cadastro")
+
+        user.password = undefined;
+        return res.send({ user });
+
+    } catch (err){
+        return res.status(400).send({ error: 'Registration Falid!'});
+
+    }
+});
+
+module.exports = app => app.use('/auth', router);
